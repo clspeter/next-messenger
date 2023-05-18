4ab1b29a116dd6516dd67da0bd78026a72aaa9c6
clspeter <mustpe@gmail.com>
2023-05-18 21:06:38 +0800

    add MobileFooter

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,8 @@
 import Sidebar from "@/app/components/sidebar/Sidebar";
-import DesktopSidebar from "../components/sidebar/DesktopSidebar";
 
 export default async function UserLayout({ children }: { children: React.ReactNode }) {
     return (
         // @ts-expect-error Server Component
-        <Sidebar>
-            <div className="h-full">
-                <DesktopSidebar />
-                <main className="h-full lg:pl-20">{children}</main>
-            </div>
-        </Sidebar>
+        <Sidebar />
     );
 }
