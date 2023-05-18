37e1ec27c04928a812e3bdc926b11ebb8f7c0f72
clspeter <mustpe@gmail.com>
2023-05-18 22:12:57 +0800

    test status idicator

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,6 +2,7 @@
 
 import { User } from "@prisma/client";
 import Image from 'next/image';
+import clsx from 'clsx';
 
 interface AvatarProps {
     user?: User;
@@ -13,6 +14,7 @@ const Avator: React.FC<AvatarProps> = ({ user }) => {
             <div className="relative inline-block h-9 w-9 overflow-hidden rounded-full md:h-11 md:w-11">
                 <Image alt="Avator" src={user?.image || '/images/placeholder.png'} fill />
             </div>
+            <span className="absolute right-0 top-0 block h-2 w-2 rounded-full bg-green-500 ring-2 ring-white md:h-3 md:w-3" />
         </div>
     );
 }
