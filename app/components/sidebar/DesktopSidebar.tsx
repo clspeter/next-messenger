b56c321b62ddb3eaad7099dbf54dfa1858b22608
clspeter <mustpe@gmail.com>
2023-05-22 00:35:55 +0800

    add setting modal

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,10 +1,15 @@
 'use client';
 
-import useRoutes from "@/app/hooks/useRoutes";
-import { useState } from "react";
-import DesktopItem from "./DesktopItem";
-import { User } from "@prisma/client";
-import Avator from "../Avatar";
+import clsx from 'clsx';
+import { useState } from 'react';
+
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import useRoutes from '@/app/hooks/useRoutes';
+import { User } from '@prisma/client';
+
+import Avator from '../Avatar';
+import DesktopItem from './DesktopItem';
+import SettingModal from './SettingModal';
 
 interface DesktopSidebarProps {
     currentUser: User;
@@ -16,28 +21,30 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="hidden justify-between lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:flex lg:w-20 lg:flex-col lg:overflow-y-auto lg:border-r-[1px] lg:bg-white lg:pb-4 xl:px-6">
-            <nav className="mt-4 flex flex-col justify-between">
-                <ul role="list" className="flex flex-col items-center space-y-1">
-                    {routes.map((item) => (
-                        <DesktopItem
-                            key={item.label}
-                            label={item.label}
-                            icon={item.icon}
-                            href={item.href}
-                            active={item.active}
-                            onClick={item.onClick}
-                        />
-                    ))}
-
-                </ul>
-            </nav>
-            <nav className="mt-4 flex flex-col items-center justify-between">
-                <div onClick={() => setIsOpen(true)}>
-                    <Avator user={currentUser} />
-                </div>
-            </nav>
-        </div>
+        <>
+            <SettingModal currentUser={currentUser} isOpen={isOpen} onClose={() => setIsOpen(false)} />
+            <div className="hidden justify-between lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:flex lg:w-20 lg:flex-col lg:overflow-y-auto lg:border-r-[1px] lg:bg-white lg:pb-4 xl:px-6">
+                <nav className="mt-4 flex flex-col justify-between">
+                    <ul role="list" className="flex flex-col items-center space-y-1">
+                        {routes.map((item) => (
+                            <DesktopItem
+                                key={item.label}
+                                label={item.label}
+                                icon={item.icon}
+                                href={item.href}
+                                active={item.active}
+                                onClick={item.onClick}
+                            />
+                        ))}
+                    </ul>
+                </nav>
+                <nav className="mt-4 flex flex-col items-center justify-between">
+                    <div className=" cursor-pointer" onClick={() => setIsOpen(true)}>
+                        <Avator user={currentUser} />
+                    </div>
+                </nav>
+            </div>
+        </>
     );
 }
 
